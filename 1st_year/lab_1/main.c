064e8207ea986ef0aa9c2ee18e1bfc918fc51dd5
Perkovec <perkovec24@gmail.com>
2016-09-21 20:09:43 +0300

    remove shitcode

--- 1st_year/lab_1/main.c
+++ 1st_year/lab_1/main.c
@@ -1,67 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 #include "main.h"
 
 int figureNum = 0;
-
-int isPolygonPoint(Point point, Point *verticles, int length) {
-  int i, j;
-  bool is_inside = false;
-  Point verticle_i, verticle_j;
-  
-  for (i = 0, j = length - 1; i < length; j = ++i) {
-    verticle_i = verticles[i];
-    verticle_j = verticles[j];
-    if (((verticle_i.y >= point.y) != (verticle_j.y >= point.y)) &&
-      (point.x <= (verticle_j.x - verticle_i.x) * (point.y - verticle_i.y) / (verticle_j.y - verticle_i.y) + verticle_i.x)) {
-        is_inside = !is_inside;
-      }
-  }
-
-  return is_inside;
-}
-
 void process1() {
   Point point;
-  Point *verticles = calloc(3, sizeof(Point));
-
-  verticles[0].x = -1.0f;
-  verticles[0].y = -1.0f;
-
-  verticles[1].x = -1.0f;
-  verticles[1].y = 1.0f;
-
-  verticles[2].x = 1.0f;
-  verticles[2].y = 1.0f;
 
   printf("Введите координаты точки: ");
   scanf("%f %f", &point.x, &point.y);
 
-  if (isPolygonPoint(point, verticles, 3) == 1) {
-    printf("Точка принадлежит фигуре 24\n");
+  if (point.x <= point.y && point.x >= -1) {
+    printf("Точка принадлежит фигуре 1\n");
   } else {
-    printf("Точка не принадлежит фигуре 24\n");
+    printf("Точка не принадлежит фигуре 1\n");
+  }
+}
+
+void process2() {
+  Point point;
+
+  printf("Введите координаты точки: ");
+  scanf("%f %f", &point.x, &point.y);
+}
+
+void process18() {
+  Point point;
+
+  printf("Введите координаты точки: ");
+  scanf("%f %f", &point.x, &point.y);
+
+  point.y = fabsf(point.y); 
+  point.x = fabsf(point.x); 
+  if (point.x >= point.y && point.x <= 1) {
+    printf("Точка принадлежит фигуре 18\n");
+  } else {
+    printf("Точка не принадлежит фигуре 18\n");
   }
 }
 
 void process24() {
   Point point;
-  Point *verticles = calloc(3, sizeof(Point));
-
-  verticles[0].x = -1.0f;
-  verticles[0].y = 0.0f;
-
-  verticles[1].x = -1.0f;
-  verticles[1].y = -1.0f;
-
-  verticles[2].x = 0.0f;
-  verticles[2].y = -1.0f;
 
   printf("Введите координаты точки: ");
   scanf("%f %f", &point.x, &point.y);
 
-  if (isPolygonPoint(point, verticles, 3) == 1) {
+  if (point.x >= point.y && point.x <= 0) {
     printf("Точка принадлежит фигуре 24\n");
   } else {
     printf("Точка не принадлежит фигуре 24\n");
@@ -77,6 +62,9 @@ int main() {
   switch(figureNum) {
     case 1:
       process1();
+    break;
+    case 18:
+      process18();
     break;
     case 24:
       process24();
